bfd0fa011bb897b07f3c10c89781ed2f2dc37a51
fabri <fabri@bbqlab.net>
2014-11-07 14:56:43 +0100

    account section

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,10 @@ from skillup.shop.views import *
 
 urlpatterns = patterns('',
     url(r"^gotodashboard/$", goto_dashboard),
+    url(r"^profile/$", profile, name='profile'),
+    url(r"^changeplan/$", change_plan, name='change_plan'),
+    url(r"^invoices/$", invoices, name='invoices'),
+    url(r"^billing/$", billing_info, name='billing'),
     url(r"^agentlogin/$", agent_login, name='agentlogin'),
     url(r"^gotoaccount/$", goto_account),
     url(r"^wait/$", wait_lhc_install)
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext, Context
 from django.conf import settings
 from django.shortcuts import render_to_response, get_object_or_404
@@ -7,7 +7,21 @@ from django.contrib.auth.models import User
 from skillup.shop.forms import *
 
 
+def profile(request):
+    return render_to_response('shop/profile.html', {'user': request.user}, 
+            context_instance=RequestContext(request))
 
+def change_plan(request):
+    return render_to_response('shop/change_plan.html', {'user': request.user}, 
+            context_instance=RequestContext(request))
+
+def invoices(request):
+    return render_to_response('shop/invoices.html', {'user': request.user}, 
+            context_instance=RequestContext(request))
+
+def billing_info(request):
+    return render_to_response('shop/billing_info.html', {'user': request.user}, 
+            context_instance=RequestContext(request))
 
 def agent_login(request):
     form = AgentLoginForm()
@@ -27,12 +41,6 @@ def agent_login(request):
         'form': form,
         'not_found': not_found
     })
-
-    path = '/perche'
-    if request.user.is_authenticated():
-        path = "%s%s" % (settings.LHC_DASHBOARD_URL, request.user.username)
-    return HttpResponseRedirect(path)
-
 
 def goto_account(request):
     path = '/'
